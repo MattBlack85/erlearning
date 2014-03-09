54deed5fa2aa85643c77d68871dd197debcabbba
MattBlack85 <promat85@gmail.com>
2014-03-09 21:11:42 +0100

    Added a function.

--- records.erl
+++ records.erl
@@ -1,9 +1,14 @@
 %% When working in a shell load record with rr(module name).
 -module(records).
--export([first_customer/0]).
+-export([first_customer/0, print_contact/1]).
 -record(customer, { name="<anonymous>", address, phone }).
 
 first_customer() ->
     #customer{name="Matt",
 	      address="67th crater, Moon",
 	      phone=-78654}.
+
+print_contact(#customer{name=Name, address=Address, phone=Phone}) when Phone =/= undefined ->
+    io:format("Contact: ~s at ~s. ~n", [Name, Phone]);
+print_contact(#customer{name=Name, address=Address, phone=Phone}) ->
+    io:format("Contact: ~s but we have no phone. ~n", [Name]).
